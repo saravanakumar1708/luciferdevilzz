baf5a7563eee92d4a759b030cf9db32c2d12d4f6
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-10-06 13:40:06 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -119,10 +119,10 @@ async def start(bot, message):
             reply_markup=InlineKeyboardMarkup(
                 [[
                 InlineKeyboardButton("🔎Search Here🔎", switch_inline_query_current_chat=''),
-                InlineKeyboardButton("🔰CHANNEL 🔰", url="https://t.me/Latest_Movie_Media")
+                InlineKeyboardButton("𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗖𝗵𝗮𝗻𝗻𝗲𝗹♻️", url="https://t.me/Latest_Movie_Media")
                 ],[
-                InlineKeyboardButton("🔰GROUP🔰", url="https://t.me/Latest_Movie_Media"),
-                InlineKeyboardButton("DEVOLOPER", url="https://t.me/Lucifer_DevilZ")
+                InlineKeyboardButton("𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗚𝗿𝗼𝘂𝗽♻️", url="https://t.me/Latest_Movie_Media"),
+                InlineKeyboardButton("𝗢𝘄𝗻𝗲𝗿 ♻️", url="https://t.me/Lucifer_DevilZ")
                 ]]
             )
         )
